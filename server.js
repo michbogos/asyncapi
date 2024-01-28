2e41e286a82c6fc65176806a3b1b7dcc8f1cc827
michbogos <bogostamicheal@gmail.com>
2024-01-28 09:41:56 +0100

    Return placeholder images

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+let images = []
+
+for(let i = 0; i < 200; i++){
+  images.push({image:`https://loremflickr.com/640/480/pill`})
+}
+
 app.get('/api/pharm/images', (req, res) => {
-  res.send('Successful response.');
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(images));
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
